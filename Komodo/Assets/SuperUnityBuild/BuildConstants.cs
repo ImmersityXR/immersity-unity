8f482410da010d965e2009ac2b4c0c553abaeb82
parseccentric <8165314+parseccentric@users.noreply.github.com>
2025-12-08 23:31:08 -0600

    add SuperUnityBuild

---
+++ Komodo/Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,47 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+namespace SuperUnityBuild.Generated
+{
+    public enum ReleaseType
+    {
+        None,
+        Development,
+    }
+
+    public enum Platform
+    {
+        None,
+        WebGL,
+    }
+
+    public enum ScriptingBackend
+    {
+        None,
+        IL2CPP,
+    }
+
+    public enum Architecture
+    {
+        None,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static class BuildConstants
+    {
+        public static readonly DateTime buildDate = new DateTime(639008257095814924);
+        public const string version = "1.0.0.1";
+        public const ReleaseType releaseType = ReleaseType.Development;
+        public const Platform platform = Platform.WebGL;
+        public const ScriptingBackend scriptingBackend = ScriptingBackend.IL2CPP;
+        public const Architecture architecture = Architecture.WebGL;
+        public const Distribution distribution = Distribution.None;
+    }
+}
+
